7402b44b173496a06001f249de9816c8757630f0
cyberhuntersolns <54077720+cyberhuntersolns@users.noreply.github.com>
2019-08-14 22:25:28 -0400

    Update and rename winlogbeat-install to winlogbeat-install.bat

--- winlogbeat-install.bat
+++ winlogbeat-install.bat
@@ -12,17 +12,12 @@ if "%hour:~0,1%"==" " set hour=0%hour:~1,1%
 if "%hour:~1,1%"=="" set hour=0%hour%
 if "%minute:~1,1%"=="" set minute=0%minute%
 set tasktime=%hour%:%minute%
-mkdir C:\ProgramData\sysmon
-pushd "C:\ProgramData\sysmon\"
-echo [+] Downloading Sysmon...
-@powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon64.exe','C:\ProgramData\sysmon\sysmon64.exe')"
-echo [+] Downloading Sysmon config...
+mkdir "C:\Program Files\winlogbeat2"
+pushd "C:\Program Files\winlogbeat2"
+echo [+] Downloading Winlogbeat...
 @powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/cyberhuntersolns/sysmon-config/develop/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/cyberhuntersolns/sysmon-config/develop/Auto_Update.bat','C:\ProgramData\sysmon\Auto_Update.bat')"
 sysmon64.exe -accepteula -i sysmonconfig-export.xml
 sc failure Sysmon64 actions= restart/10000/restart/10000// reset= 120
 echo [+] Sysmon Successfully Installed!
-echo [+] Creating Auto Update Task set to Daily..
-SchTasks /Create /RU SYSTEM /RL HIGHEST /SC DAILY /TN Update_Sysmon_Rules /TR C:\ProgramData\sysmon\Auto_Update.bat /F /ST %tasktime%
 timeout /t 10
 exit
