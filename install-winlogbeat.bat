a5f9c6c91cde226d6db0ee2708ed8f0e3ed3a712
cyberhuntersolns <54077720+cyberhuntersolns@users.noreply.github.com>
2019-08-25 22:42:24 -0400

    Update install-winlogbeat.bat

--- install-winlogbeat.bat
+++ install-winlogbeat.bat
@@ -12,6 +12,8 @@ if "%hour:~0,1%"==" " set hour=0%hour:~1,1%
 if "%hour:~1,1%"=="" set hour=0%hour%
 if "%minute:~1,1%"=="" set minute=0%minute%
 set tasktime=%hour%:%minute%
+echo [+] Downloading Cert...
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/cyberhuntersolns/threathunting/develop/COMODORSADomainValidationSecureServerCA.crt','C:\ProgramData\Filebeat\COMODORSADomainValidationSecureServerCA.crt')"
 echo [+] Downloading Winlogbeat...
 @powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/cyberhuntersolns/threathunting/develop/winlogbeat.zip','C:\Program Files\winlogbeat.zip')"
 @powershell Expand-Archive -Force 'C:\Program Files\winlogbeat.zip' 'C:\Program Files\'"
